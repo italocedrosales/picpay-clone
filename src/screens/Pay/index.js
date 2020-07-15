23d42e550f3623a776531934e1551fd3703764c6
Italo Cedro <italocedro@pop-os.localdomain>
2020-07-15 18:50:35 -0300

    Criação do tab navigation e header do app

---
+++ src/screens/Pay/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+function Pay() {
+  return (
+    <View>
+      <Text>Pay</Text>
+    </View>
+  );
+}
+
+export default Pay;
---
+++ src/screens/Wallet/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+function Wallet() {
+  return (
+    <View>
+      <Text>Wallet</Text>
+    </View>
+  );
+}
+
+export default Wallet;
