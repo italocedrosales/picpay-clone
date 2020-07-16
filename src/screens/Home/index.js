263933994bc4a3034d22aba5cc3de954241dd336
Italo Cedro <italocedro@pop-os.localdomain>
2020-07-15 21:21:19 -0300

    Criação do componente Suggestion

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 
-import { Wrapper, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
+import { Wrapper, Container, Header, BalanceContainer, BalanceTitle, Balance } from './styles';
+
+import Suggestions from '../../components/Suggestions';
 
 export default function Home() {
   return (
     <Wrapper>
-      <Header>
-        <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
-      
-        <BalanceContainer>
-          <BalanceTitle>Meu saldo</BalanceTitle>
-          <Balance>R$ 0,00</Balance>
-        </BalanceContainer>
-      
-        <AntDesign name="gift" size={30} color="#10c86e" />
-      </Header>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e" />
+        
+          <BalanceContainer>
+            <BalanceTitle>Meu saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+        
+          <AntDesign name="gift" size={30} color="#10c86e" />
+        </Header>
+
+        <Suggestions />
+      </Container>
     </Wrapper>
   );
 }
